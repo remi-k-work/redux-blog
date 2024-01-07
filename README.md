e918442e4f43c721630fc1bda723dc3b9e7fe600
Remi <remi.k.work@gmail.com>
2024-01-07 19:55:33 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Redux Blog
+
+Building a simple social network feed app inspired by Mark Erikson's "Redux Essentials" tutorial from the official Redux documentation.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+    <div className="main-grid">
+      <header>Header</header>
+      <main>Main</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
